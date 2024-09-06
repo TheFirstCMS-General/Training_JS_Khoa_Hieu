9c8134b46fb8b7c72e1a98a232a64e3c05454b44
HueiWei <hieuanhhp124@gmail.com>
2024-09-06 07:18:22 +0700

    Add pay function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-//Get data form json
+// import student from './data.json' with {type: 'json'};
+// import detail_money from './detail_money.json' with {type: 'json'}
+
 var student = [];
-var data = await fetch('./data.json')
+await fetch('./data.json')
     .then(response => response.json())
     .then(data => {
         const arrayData = data.map(item => item);
@@ -8,29 +10,104 @@ var data = await fetch('./data.json')
     })
     .catch(error => console.error('Error:', error));
 
-const exportExcelButton = document.getElementById('exportExcelButton');
-exportExcelButton.addEventListener('click', ExportExcel);
+var detail_money = [];
+await fetch('./detail_money.json')
+    .then(response => response.json())
+    .then(data => {
+        const arrayData = data.map(item => item);
+        detail_money.push(...arrayData);
+    })
+    .catch(error => console.error('Error:', error));
 
-const addButton = document.getElementById('Add');
-addButton.addEventListener('click', AddStudent);
+//Render Data Table
+student.forEach(obj => {
+    let money = 0;
+    let DetailMoney = detail_money.find(item => item.Number == obj.Number).DetailMoney;
+    DetailMoney.forEach(item => {
+        money += item.Money;
+    })
+    obj.Money = money;
+    AddElementToTable(obj)
+});
 
-student.forEach(obj => AddElementToTable(obj));
+function initEvent(tr) {
+    let deleteButton = tr.querySelectorAll('.delete');
+    let saveButton = tr.querySelectorAll('.save');
+    let detailMoneyButton = tr.querySelectorAll('.detail_money');
+
+    deleteButton.forEach(item => {
+        item.addEventListener('click', function (event) {
+            const clickedElement = event.target;
+            const parentElement = clickedElement.parentElement.parentElement.parentElement;
+            let id = parentElement.querySelector('.identify').value;
+            student = student.filter(item => item.Number != id);
+            removeParentElement(parentElement);
+        });
+    })
+    saveButton.forEach(item => {
+        item.addEventListener('click', function (event) {
+            const clickedElement = event.target;
+            const parentElement = clickedElement.parentElement.parentElement.parentElement;
+            let id = parentElement.querySelector('.identify').value;
+            let name = parentElement.querySelector('.name').value;
+            let gender = parentElement.querySelector('.gender').value;
+            let money = parentElement.querySelector('.money').value;
+            let status = parentElement.querySelector('.status').value;
+            let dateofbirth = parentElement.querySelector('.dateofbirth').value;
+            let address = parentElement.querySelector('.address').value;
+    
+            student.forEach(item => {
+                if (item.Number == id) {
+                    item.Name = name;
+                    item.Gender = gender;
+                    item.Money = money;
+                    item.Status = status;
+                    item.DateOfBirth = dateofbirth;
+                    item.Address = address;
+                }
+            })
+            saveDataToJsonFile(student);
+        });
+    })
+    detailMoneyButton.forEach(item => {
+        item.addEventListener('click', function (event) {
+            const clickedElement = event.target;
+            const parentElement = clickedElement.parentElement.parentElement;
+            let id = parentElement.querySelector('.identify').value;
+            
+            let obj = detail_money.find(item => item.Number == id);
+            console.log(obj);
+            if(obj == undefined) {
+                alert("Chua dong tien");
+                return;
+            }
+            let DetailMoney = obj.DetailMoney;
+            console.log(DetailMoney);
+            let message = "";
+            DetailMoney.forEach(item => {
+                message += item.Phase + ": " + item.Money + ": " + item.Date + "\n";
+            })
+            alert(message)
+        });
+    })
+}
 
 function AddElementToTable(obj) {
     const tr = document.createElement('tr');
     tr.className = `tr${obj.Number}`;
     tr.innerHTML = `
             <th scope="row">
-                <input class = "content_row number${obj.Number} identify" value=${obj.Number} readonly = "true">
+                <input class = "identify content_row number${obj.Number}" value=${obj.Number} readonly = "true">
             </th>
-            <td>
+            <td >
                 <input class = "content_row name" value=${obj.Name}>
             </td>
             <td>
                 <input class = "content_row gender" value=${obj.Gender}>
             </td>
             <td>
-                <input class = "content_row money" value=${obj.Money}>
+                <input class = "content_row money money${obj.Number}" value=${obj.Money}>
+                <button class = "detail_money">Detail</button>
             </td>
             <td>
                 <input class = "content_row status" value=${obj.Status}>
@@ -43,109 +120,30 @@ function AddElementToTable(obj) {
             </td>
             <td>
                 <div class = "button_commands">
-                    <button class = "delete button_command" >Delete</button>
+                    <button class = "delete button_command">Delete</button>
                     <button class = "save button_command" >Save</button>
                 </div>
             </td>
     `;
     document.getElementById('body_table').appendChild(tr);
+    initEvent(tr);
 }
 
-let deleteButton = document.querySelectorAll('.delete');
-deleteButton.forEach(item => {
-    item.addEventListener('click', function (event) {
-        const clickedElement = event.target;
-        const parentElement = clickedElement.parentElement.parentElement;
-        let id = parentElement.querySelector('.identify').value;
-        student.remove(item => item.Number = id);
-        removeParentElement(parentElement);
-    });
-})
-function removeParentElement(element) {
-    if (element && element.parentNode) {
-        element.parentNode.removeChild(element);
-    }
-}
+
+const exportExcelButton = document.getElementById('exportExcelButton');
+exportExcelButton.addEventListener('click', ExportExcel);
+
+const addButton = document.getElementById('add');
+addButton.addEventListener('click', AddStudent);
 
 let payButton = document.getElementById('pay');
+payButton.addEventListener('click', PayMonney);
 
-payButton.addEventListener('click', function (event) {
-    let id;
-    while (true) {
-        id = prompt("Please enter your id student:");
-        if (id !== null && !isNaN(id)) {
-            break;
-        }
-    }
-    let money;
-    while (true) {
-        money = prompt("Please enter your id student:");
-        if (money !== null && !isNaN(money)) {
-            break;
-        }
-    }
-    let idTr = document.querySelectorAll(`tr${id}`);
-});
-
-function removeParentElement(element) {
-    if (element && element.parentNode) {
-        element.parentNode.removeChild(element);
-    }
-}
-
-let saveButton = document.querySelectorAll('.save');
-saveButton.forEach(item => {
-    item.addEventListener('click', function (event) {
-        const clickedElement = event.target;
-        const parentElement = clickedElement.parentElement.parentElement;
-        let id = parentElement.querySelector('.identify').value;
-        let name = parentElement.querySelector('.name').value;
-        let gender = parentElement.querySelector('.gender').value;
-        let money = parentElement.querySelector('.money').value;
-        let status = parentElement.querySelector('.status').value;
-        let dateofbirth = parentElement.querySelector('.dateofbirth').value;
-        let address = parentElement.querySelector('.address').value;
-
-        student.forEach(item => {
-            if (item.Number == id) {
-                item.Name = name;
-                item.Gender = gender;
-                item.Money = money;
-                item.Status = status;
-                item.DateOfBirth = dateofbirth;
-                item.Address = address;
-            }
-        })
-        saveDataToJsonFile(student);
-    });
-})
-
-
-
-
-
-
-async function saveDataToJsonFile(data) {
-    const jsonData = JSON.stringify(data);
-
-    try {
-        fs.writeFile('./data.json', jsonData);
-        console.log('Dữ liệu đã được ghi vào file data.json');
-    } catch (err) {
-        console.error(err);
-    }
-}
-
+//Add Button
 function AddStudent() {
     let name = prompt("Enter your name:");
     let gender = prompt("Enter your gender:");
-    let money;
-    while (true) {
-        money = prompt("Please enter your money:");
-        if (money !== null && !isNaN(money)) {
-            break;
-        }
-    }
+    let money = 0;
     let status = prompt("Enter your status:");
     let dateofbirth = prompt("Enter your date:");
     let address = prompt("Enter your address:");
@@ -161,6 +159,57 @@ function AddStudent() {
 
     AddElementToTable(obj);
     student.push(obj);
+    console.log(detail_money);
+}
+
+function removeParentElement(element) {
+    if (element && element.parentNode) {
+        element.parentNode.removeChild(element);
+    }
+}
+
+function PayMonney() {
+    let id = GetNumberInput(3, "Please enter your id student:");
+
+    let obj = student.find(item => item.Number == id);
+    if(obj == undefined) {
+        alert("Can't find student with id: "+ id);
+        return;
+    }
+
+    let money = GetNumberInput(3, "Please enter your money:");
+
+    console.log(id);
+    console.log(detail_money);
+    let a = detail_money.find(item => item.Number = id);
+    console.log(a);
+    // if(!a) {
+    //     let newDetailMoney = new Object();
+    //     newDetailMoney.Number = id;
+    //     let DetailMoney = [];
+    //     DetailMoney.push({"Phase" : 1, "Money" : money, "Date": (new Date()).toDateString});
+    //     newDetailMoney.DetailMoney = DetailMoney;
+    //     console.log(newDetailMoney);
+    //     detail_money.push(newDetailMoney);
+    // }
+
+
+    let tr = document.querySelector(`.money${id}`);
+    tr.value = money;
+
+    
+}
+
+async function saveDataToJsonFile(data) {
+    const jsonData = JSON.stringify(data);
+    console.log(data);
+    const input = createReadStream('./data.json');
+    input.pipe(output);
+
+    input.on('data', (chunk) => {
+        console.log('Data chunk received:', chunk.toString());
+    });
+    input.on('end', () => input.destroy());
 }
 
 async function ExportExcel() {
@@ -187,4 +236,19 @@ async function ExportExcel() {
         URL.revokeObjectURL(link.href);
         link.remove();
     });
+}
+
+function GetNumberInput(count, message) {
+    let obj;
+    while (true) {
+        if (count == 0) {
+            break;
+        }
+        obj = prompt(message);
+        if (obj !== null && !isNaN(obj)) {
+            break;
+        }
+        count--;
+    }
+    return obj;
 }
